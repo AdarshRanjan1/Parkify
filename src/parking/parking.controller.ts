7e14c56307e9cf05b193f8994248319c2eff36a9
Adarsh Ranjan <adarshranjan8427@gmail.com>
2025-04-24 16:02:40 +0530

    Implemented the park endpoint - This allocates the nearest available slot

--- src/parking/parking.controller.ts
+++ src/parking/parking.controller.ts
@@ -6,15 +6,26 @@ import { ParkingService } from './parking.service';
 export class ParkingController {
   constructor(private readonly parkingService: ParkingService) {}
 
+  // Router for the initial setting of the total slots
   @Post('init')
   initializeParking(@Body('totalSlots') totalSlots: number) {
     console.log('Received slots:', totalSlots); // This console will check if the array is getting initialized or not
     return this.parkingService.initializeParking(totalSlots);
   }
 
+  // For the increment of slots
   @Patch('lot')
   addSlots(@Body('increment_slot') incrementSlot: number) {
     return this.parkingService.addSlots(incrementSlot);
   }
   
+
+  // For allocating the slots
+  @Post('park')
+  parkVehicle(
+    @Body('vehicle_number') vehicleNumber: string,
+    @Body('color') color: string,
+  ) {
+    return this.parkingService.parkVehicle(vehicleNumber, color);
+  }
 }
--- src/parking/parking.service.ts
+++ src/parking/parking.service.ts
@@ -1,24 +1,65 @@
 import { Injectable } from '@nestjs/common';
+import MinHeap  from 'heap-js'; 
+// I will use min heap to optimize 
+// the time complexity of finding the first 
+// free slot doing a linear search would take O(n) time but with
+// min heap it will just take O(log n)
 
 //Injectable is used to mark the class as a service that can be use elsewhere
 @Injectable()
 export class ParkingService {
-  private parkingSlots: (string | null)[] = [];
+  private parkingSlots: (string | null)[] = []; //Stores the no of slots
+  private availableSlots = new MinHeap<number>(); // Stores available no of slots
+  private occupiedSlots = new Map<number, { vehicle_number: string; color: string }>(); // Stores occupied no of slots
+  // private occupiedSlots = new Map<number, string>();
 
   // This one is for initializing the size of the parking lot in the beginning
   initializeParking(totalSlots: number) {
     this.parkingSlots = Array(totalSlots).fill(null);
+    this.availableSlots = new MinHeap<number>();
+    for (let i = 1; i <= totalSlots; i++) {
+      this.availableSlots.push(i);
+    }
     return { message: `Parking lot initialized with ${totalSlots} slots.` };
   }
   
   // This is for increasing the size of the parking lot if necessary
   addSlots(incrementSlot: number) {
-    this.parkingSlots.push(...Array(incrementSlot).fill(null));
+    const currentTotal = this.parkingSlots.length; // Existing total
+    const newSlots = Array(incrementSlot).fill(null);
+    this.parkingSlots.push(...newSlots);
+  
+    // Push new slot indices into the MinHeap (1-based indexing)
+    for (let i = currentTotal + 1; i <= this.parkingSlots.length; i++) {
+      this.availableSlots.push(i);
+    }
+  
     console.log('Total slots after incrementing:', this.parkingSlots.length);
     return { total_slot: this.parkingSlots.length };
   }
 
   getSlots() {
     return this.parkingSlots;
+  }
+
+  // This endpoint is used for checking if slot is available or not and then appointing the slot 
+  parkVehicle(vehicle_number: string, color: string) {
+    if (this.availableSlots.size() === 0) {
+      return { message: 'No available slots' };
+    }
+  
+    const slotNumber = this.availableSlots.pop();
+  
+    if (slotNumber === undefined) {
+      return { message: 'Error: No slot returned from heap.' };
+    }
+  
+    // Store both vehicle number and color
+    this.occupiedSlots.set(slotNumber, { vehicle_number, color });
+  
+    return {
+      message: 'Vehicle parked successfully',
+      slot_number: slotNumber,
+    };
   }
 }
